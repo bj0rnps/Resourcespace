bcef3aabe9848c790641f763eb5dc2f775d34007
Bjørn-Ole Johansen <55537500+bj0rnps@users.noreply.github.com>
2022-11-09 14:08:53 +0100

    Update 02createuser-rw.sql

--- Resourcespace/mysql/02createuser-rw.sql
+++ Resourcespace/mysql/02createuser-rw.sql
@@ -1,2 +1,2 @@
-CREATE USER 'resourcespace_rw'@'%' IDENTIFIED BY 'TopSecret';
-CREATE USER 'resourcespace_r'@'%' IDENTIFIED BY 'TopSecret';
+CREATE USER 'resourcespace_rw'@'%' IDENTIFIED BY 'Changeme!';
+CREATE USER 'resourcespace_r'@'%' IDENTIFIED BY 'Changeme!';
